2a88d55f4ece4ad893f06a02ea484d6a0b64f33d
ku08070807-create <ku08070807@gmail.com>
2026-03-27 20:47:48 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,6 +1,9 @@
 let num = 15;
 
-if (num % 3 === 0) {
+if (num % 3 === 0 && num % 5 === 0) {
+    console.log('3と5の倍数です');
+}
+else if (num % 3 === 0) {
     console.log('3の倍数です');
 }
 
@@ -8,11 +11,6 @@ else if (num % 5 === 0) {
     console.log('5の倍数です');
 }
 
-else if (num % 3 === 0 && num % 5 === 0) {
-    console.log('3と5の倍数です');
-}
-
 else {
     console.log(num);
-}
-
+}  
