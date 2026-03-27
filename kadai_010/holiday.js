54be813a62da5dc0a79c7da8ff8235f85c12bed9
ku08070807-create <ku08070807@gmail.com>
2026-03-28 08:58:24 +0900

    Add files via upload

--- kadai_010/holiday.js
+++ kadai_010/holiday.js
@@ -1,10 +1,29 @@
 const holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"]
 
 // for文の場合
-for (  let i  = 1; i <= 10; i++ )
- { console.log(i);}
+for (  let i  = 1; i < 16; i++ )
+ { console.log(holidays[i]);}
 
 // while文の場合
-while (let i = 1; i <= 10; i++) {
-  console.log(i); 
-}   
+let i = 1;
+while (i < 16) {
+  console.log(holidays[i]); 
+  i++;  
+} 
+
+console.log(holidays[1]);
+console.log(holidays[2]);
+console.log(holidays[3]);
+console.log(holidays[4]);
+console.log(holidays[5]);
+console.log(holidays[6]);
+console.log(holidays[7]);
+console.log(holidays[8]);
+console.log(holidays[9]);
+console.log(holidays[10]);
+console.log(holidays[11]);
+console.log(holidays[12]);
+console.log(holidays[13]);
+console.log(holidays[14]); 
+console.log(holidays[15]);
+console.log(holidays[16]);
