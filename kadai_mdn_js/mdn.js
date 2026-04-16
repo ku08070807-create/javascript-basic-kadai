b50da7faec7e40ce3c2f15a86ef8f79c445656c1
ku08070807-create <ku08070807@gmail.com>
2026-04-16 13:03:45 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,8 +1,4 @@
-const 年 = year => new Date(year, 0, 1);
-const　月 = (year, month) => new Date(year, month - 1, 1);
-const 日 = (year, month, day) => new Date(year, month - 1, day);
-console.log(年(2024)); // 2024-01-01T00:00:00.000Z
-console.log(月(2024, 6)); 
-// 2024-05-31T23:00:00.000Z
-console.log(日(2024, 6, 1)); 
-// 2024-06-01T00:00:00.000Z
+const 年 = year => getFullYear(year);
+const　月 = month=>getMonth(month); 
+const 日 = getDate => getDate(date);
+console.log(年(2024年 10月 12 日)
