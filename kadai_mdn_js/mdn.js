d6f21352a12d8297ba256f3416482fc9e1ca2963
ku08070807-create <ku08070807@gmail.com>
2026-04-16 12:53:18 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,2 +1,8 @@
-console.log(new Date(8.64e15).toString()); // "Sat Sep 13 275760 00:00:00 GMT+0000 (Coordinated Universal Time)"
-console.log(new Date(8.64e15 + 1).toString()); // "Invalid Date"
+const 年 = year => new Date(year, 0, 1);
+const　月 = (year, month) => new Date(year, month - 1, 1);
+const 日 = (year, month, day) => new Date(year, month - 1, day);
+console.log(年(2024)); // 2024-01-01T00:00:00.000Z
+console.log(月(2024, 6)); 
+// 2024-05-31T23:00:00.000Z
+console.log(日(2024, 6, 1)); 
+// 2024-06-01T00:00:00.000Z
