60151be8f0f82b8939d44f971d3ab4ca429503c2
ku08070807-create <ku08070807@gmail.com>
2026-04-16 15:49:46 +0900

    Add files via upload

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -2,9 +2,7 @@ const btn = document.getElementById('btn');
 const text = document.getElementById('text');
 
 btn.addEventListener('click', () => {
-  text.textContent = 'ボタンがクリックされました！';
-});
-
 setTimeout(() => {
-  text.textContent = '2秒後に表示します！';
+  text.textContent = 'ボタンをクリックしました！';
 }, 2000);
+});
