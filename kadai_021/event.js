f4fb0850fe03f4ca08c3a5e5d25a951dcb7082b8
ku08070807-create <ku08070807@gmail.com>
2026-04-16 15:25:01 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById('');
+const text = document.getElementById('');
+
+btn.addEventListener('', () => {
+
+});
+
+setTimeout(() => {
+  console.log('処理（非同期処理）');
+}, 2);
